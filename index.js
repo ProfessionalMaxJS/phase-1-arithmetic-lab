c1865e7ed26f5cad8f92d4f681b9f660ddb696a6
Max <max.edison@yahoo.com>
2021-10-12 21:46:57 -0400

    commit tree d3230d473b57214d0754a07db639935c9e1439e2

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // Write your code here
+
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+// multiply is an equation whose multiplied results will equal 62
+
+const random = (Math.floor(Math.random())*100)+1;
+// random generates a random integer greater than 0
+
+const num3 = 64;
+const num4 = 10;
+const mod = num3%num4;
+// mod is an equation that calculates a remainder that is equal to 4
+
+const num5 = (Math.floor(Math.random()*20));
+const num6 = (Math.floor(Math.random()*20));
+const num7 = (Math.floor(Math.random()*20));
+const num8 = (Math.floor(Math.random()*20));
+const num9 = (Math.floor(Math.random()*20));
+const num10 = 20;
+const max = Math.max(num5, num6, num7, num8, num9, num10);
+// max will return 20 as the highest number in the set
